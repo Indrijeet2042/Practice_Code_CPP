d2c1496ecc80bbea74c566dd2646fa6b0433520f
Indrijeet Kumar <kumarindrijeet2042@gmail.com>
2023-08-08 23:34:03 +0530

    comment added

---
+++ PrintMatrix.cpp/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  PrintMatrix.cpp
+//
+//  Created by Anurag kumar on 22/05/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+    
+    int num;
+    int mat[2][2];
+    cout<<"Enter the dimention of the matrix"<<" :"<<" ";
+    cin>>num;
+    cout<<"Enter the elements of the matrix"<<" :"<<" ";
+    for(int i=0;i<num;i++)
+        for(int j=0;j<num;j++)
+            cin>>mat[i][j];
+    
+    for(int l=0;l<num;l++)
+        for(int m=0;m<num;m++)
+            cout<<mat[l][m]<<endl;
+    return 0;
+}
